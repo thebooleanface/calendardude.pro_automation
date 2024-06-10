9a1e08d7681bd119e62d015f8e893a75932f6822
0/1 <156817923+thebooleanface@users.noreply.github.com>
2024-06-10 20:03:24 +0530

    Added image for percentage of year passed

--- final.py
+++ final.py
@@ -26,6 +26,35 @@ secret_key = ''
 totp = pyotp.TOTP(secret_key)
 current_otp = totp.now()
 
+#image for percentage of year passed 
+img2 = Image.new('RGB',(400,300),color='black')
+draw = ImageDraw.Draw(img2)
+red = (255,0,0,1)
+white = (255,255,255,1)
+
+cur = datetime.datetime.now()
+start_of_year = datetime.datetime(cur.year,1,1)
+days = (cur - start_of_year).days
+end = datetime.datetime(cur.year, 12,31)
+total = (end-start_of_year).days
+
+perc = (days*300)/total
+
+#the width of the rectangle is 300 pixels with start at 50 and end at 350
+red_coords = ((50,100),(perc,150))
+black_coords = ((perc,100),(300,150))
+
+draw.rectangle(red_coords,fill=red)
+draw.rectangle(black_coords,fill=black)
+
+text = f"{perc/3} year is gone..."
+text_coord =  (perc,160)
+text_color = 'red'
+font = ImageFont.truetype("Helvetica", 10)
+draw.text(text_coord,text,fill='red',font=font)
+
+img2.save('rectangle_image.png')
+
 cl = Client()
 cl.delay_range = [1, 10]
 cl.load_settings("session.json")
